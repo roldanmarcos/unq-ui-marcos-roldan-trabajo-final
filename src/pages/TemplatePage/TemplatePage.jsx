5120dbf2a4215c66459404baf1c2ae7e78b7db20
roldanmarcos <guillermoroldan0@gmail.com>
2024-11-23 00:23:12 -0300

    proyecto con canvas más limpio

---
+++ src/pages/TemplatePage/TemplatePage.jsx
@@ -0,0 +1,15 @@
+import Header from '../../components/Header/Header'
+import './TemplatePage.css'
+
+function TemplatePage() {
+    return (
+        <div className='page-container'>
+            <Header />
+            <div className='body-container'>
+            <div className='object'></div>
+            </div>
+        </div>
+    )
+}
+
+export default TemplatePage
