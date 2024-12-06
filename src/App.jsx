8e13267b4e220367b249fee65c20a3d67ddd739e
roldanmarcos <guillermoroldan0@gmail.com>
2024-12-06 00:19:22 -0300

    dependencia y cosas necesarias para navegar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import TemplatePage from './pages/TemplatePage/TemplatePage'
+import WelcomePage from './pages/WelcomePage/WelcomePage'
+import OptionsPage from './pages/OptionsPage/OptionPage';
 
 function App() {
     return (
-        <TemplatePage />
-    )
+        <Router>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path='options' element={<OptionsPage />} />
+            {/* <Route path='game' element={<OptionPage />} /> */}
+          </Routes>
+        </Router>
+    );
 }
 
 export default App
