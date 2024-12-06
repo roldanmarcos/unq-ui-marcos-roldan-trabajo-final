67bd1ff3f51202bbf8549a8d4df61b85849a6841
roldanmarcos <guillermoroldan0@gmail.com>
2024-12-06 00:18:07 -0300

    se modifica TemplatePage
    se modifica el componente TemplatePage para que pueda ser reutilizable en otras páginas

--- src/pages/TemplatePage/TemplatePage.jsx
+++ src/pages/TemplatePage/TemplatePage.jsx
@@ -1,18 +1,15 @@
 import Header from '../../components/Header/Header'
-import OptionsMenu from '../../components/OptionsMenu/OptionsMenu'
-import Tablero from '../../components/Tablero/Tablero'
 import './TemplatePage.css'
 
-function TemplatePage() {
-    return (
-        <div className='page-container'>
-            <Header />
-            <div className='body-container'>
-                <OptionsMenu />
-                <Tablero />
-            </div>
-        </div>
-    )
+const TemplatePage = ({ children }) => {
+  return (
+    <div className='page-container'>
+      <Header />
+      <div className='body-container'>
+        {children}
+      </div>
+    </div>
+  );
 }
 
 export default TemplatePage
