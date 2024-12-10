c4f79b43fa091db448452fb2ec2625090d857c94
roldanmarcos <guillermoroldan0@gmail.com>
2024-12-10 00:26:35 -0300

    se agrega una libreria para confetti
    se muestra un confetti cuando finaliza una partida

--- src/components/CongratulationsModal/CongratulationsModal.jsx
+++ src/components/CongratulationsModal/CongratulationsModal.jsx
@@ -1,7 +1,33 @@
 import './CongratulationsModal.css';
 import Button from '../Button/Button';
+import { useEffect } from 'react';
 
 const CongratulationsModal = ({ title, subtitle, onRestart, onChangeMode }) => {
+
+    useEffect(() => {
+        // Crea un lienzo personalizado
+        const myCanvas = document.createElement('canvas');
+        document.body.appendChild(myCanvas);
+        myCanvas.classList.add('confetti-canvas');
+
+        const myConfetti = confetti.create(myCanvas, {
+            resize: true, // Ajusta el tamaño del lienzo al cambiar el tamaño de la ventana
+            useWorker: true, // Mejora el rendimiento en navegadores modernos
+        });
+
+        // Lanza confet
+        myConfetti({
+            particleCount: 100,
+            spread: 100,
+            origin: { y: 0.4 },
+        });
+
+        // Limpia el lienzo al desmontar el componente
+        return () => {
+            myCanvas.remove();
+        };
+    }, []);
+
     return (
         <div className="modal-overlay">
             <div className="modal-content">
