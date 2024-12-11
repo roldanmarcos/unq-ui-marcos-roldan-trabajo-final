a89e75e72df9b4673015a8ee92b81e0d5ea9dd11
roldanmarcos <guillermoroldan0@gmail.com>
2024-12-11 12:09:22 -0300

    cambio de imagenes

--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -1,73 +1,73 @@
-import beer from '../../assets/cardImages/beer.svg';
-import beet from '../../assets/cardImages/beet.svg';
-import bread from '../../assets/cardImages/bread.svg';
-import celery from '../../assets/cardImages/celery.svg';
-import cheese from '../../assets/cardImages/cheese.svg';
-import cherry from '../../assets/cardImages/cherry.svg';
-import cocktail from '../../assets/cardImages/cocktail.svg';
-import cookies from '../../assets/cardImages/cookies.svg';
-import corn from '../../assets/cardImages/corn.svg';
-import cucumber from '../../assets/cardImages/cucumber.svg';
-import cup from '../../assets/cardImages/cup.svg';
-import cupcake from '../../assets/cardImages/cupcake.svg';
-import eggs from '../../assets/cardImages/eggs.svg';
-import hamburger from '../../assets/cardImages/hamburger.svg';
-import hazelnut from '../../assets/cardImages/hazelnut.svg';
-import honey from '../../assets/cardImages/honey.svg';
-import ingredients from '../../assets/cardImages/ingredients.svg';
-import kiwi from '../../assets/cardImages/kiwi.svg';
-import macaron from '../../assets/cardImages/macaron.svg';
-import melon from '../../assets/cardImages/melon.svg';
-import milk from '../../assets/cardImages/milk.svg';
-import nachos from '../../assets/cardImages/nachos.svg';
-import pancake from '../../assets/cardImages/pancake.svg';
-import pear from '../../assets/cardImages/pear.svg';
-import pie from '../../assets/cardImages/pie.svg';
-import pineapple from '../../assets/cardImages/pineapple.svg';
-import pizza from '../../assets/cardImages/pizza.svg';
-import plum from '../../assets/cardImages/plum.svg';
-import porridge from '../../assets/cardImages/porridge.svg';
-import potato from '../../assets/cardImages/potato.svg';
-import pretzel from '../../assets/cardImages/pretzel.svg';
-import radish from '../../assets/cardImages/radish.svg';
-import raspberry from '../../assets/cardImages/raspberry.svg';
-import steak from '../../assets/cardImages/steak.svg';
+import announceVoice from '../../assets/cardImages/announce-voice.svg';
+import backgroundMusic from '../../assets/cardImages/background-music.svg';
+import badgeAward from '../../assets/cardImages/badge-award.svg';
+import badge from '../../assets/cardImages/badge.svg';
+import books from '../../assets/cardImages/books.svg';
+import box from '../../assets/cardImages/box.svg';
+import businessAttireTie from '../../assets/cardImages/business-attire-tie.svg';
+import calendar from '../../assets/cardImages/calendar.svg';
+import carton from '../../assets/cardImages/carton.svg';
+import computer from '../../assets/cardImages/computer.svg';
+import cv from '../../assets/cardImages/cv.svg';
+import deliveryPackage from '../../assets/cardImages/delivery-package.svg';
+import email from '../../assets/cardImages/e-mail.svg';
+import emptyBox from '../../assets/cardImages/empty-box.svg';
+import eraser from '../../assets/cardImages/eraser.svg';
+import folder from '../../assets/cardImages/folder.svg';
+import garbageBin from '../../assets/cardImages/garbage-in-trash-bin-recycle-bin-delete.svg';
+import highlighter from '../../assets/cardImages/highlighter.svg';
+import importantDocuments from '../../assets/cardImages/important-documents.svg';
+import lifeBuoy from '../../assets/cardImages/life-buoy.svg';
+import listList from '../../assets/cardImages/list-list.svg';
+import lock from '../../assets/cardImages/lock.svg';
+import pencilSharpener from '../../assets/cardImages/pencil-sharpener.svg';
+import pencil from '../../assets/cardImages/pencil.svg';
+import pen from '../../assets/cardImages/pen.svg';
+import planPlane from '../../assets/cardImages/plan-plane.svg';
+import safety from '../../assets/cardImages/safety.svg';
+import setUp from '../../assets/cardImages/set-up.svg';
+import target from '../../assets/cardImages/target.svg';
+import thinkObserve from '../../assets/cardImages/think-observe.svg';
+import toChatWith from '../../assets/cardImages/to-chat-with.svg';
+import toolsForRepairing from '../../assets/cardImages/tools-for-reparing.svg';
+import usbDataTransfer from '../../assets/cardImages/usb-data-transfer.svg';
+import work from '../../assets/cardImages/work.svg';
 
 const images = [
-    beer,
-    beet,
-    bread,
-    celery,
-    cheese,
-    cherry,
-    cocktail,
-    cookies,
-    corn,
-    cucumber,
-    cup,
-    cupcake,
-    eggs,
-    hamburger,
-    hazelnut,
-    honey,
-    ingredients,
-    kiwi,
-    macaron,
-    melon,
-    milk,
-    nachos,
-    pancake,
-    pear,
-    pie,
-    pineapple,
-    pizza,
-    plum,
-    porridge,
-    potato,
-    pretzel,
-    radish,
-    raspberry,
-    steak,
+    announceVoice,
+    backgroundMusic,
+    badgeAward,
+    badge,
+    books,
+    box,
+    businessAttireTie,
+    calendar,
+    carton,
+    computer,
+    cv,
+    deliveryPackage,
+    email,
+    emptyBox,
+    eraser,
+    folder,
+    garbageBin,
+    highlighter,
+    importantDocuments,
+    lifeBuoy,
+    listList,
+    lock,
+    pencilSharpener,
+    pencil,
+    pen,
+    planPlane,
+    safety,
+    setUp,
+    target,
+    thinkObserve,
+    toChatWith,
+    toolsForRepairing,
+    usbDataTransfer,
+    work,
 ];
 
 export default images;
