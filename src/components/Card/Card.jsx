9157120b03fddbafe08bf1c169522c1c2577659e
roldanmarcos <guillermoroldan0@gmail.com>
2024-12-11 10:53:23 -0300

    ahora las cartas matcheadas quedan en color verde

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,11 @@ import './Card.css';
 
 const Card = ({ card, flipped, matched, onClick }) => {
     return (
-        <div className={`card ${flipped || matched ? 'flipped' : ''}`} onClick={onClick}>
+        <div className={`card ${flipped || matched ? 'flipped' : ''} ${matched ? 'matched' : ''}`} onClick={onClick}>
             {flipped || matched ? card : '❓'}
         </div>
     );
 };
+
 
 export default Card;
