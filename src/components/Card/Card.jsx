9369013e51eb7c83134c2f1f5b99da4bf70a7283
roldanmarcos <guillermoroldan0@gmail.com>
2024-12-11 15:34:47 -0300

    se cambia emoji ? por una imagen

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,16 @@
 import './Card.css';
+import questionIcon from '../../assets/question.svg';
 
 const Card = ({ card, flipped, matched, onClick }) => {
     return (
         <div className={`card ${flipped || matched ? 'flipped' : ''} ${matched ? 'matched' : ''}`} onClick={onClick}>
-            {flipped || matched ? card : '❓'}
+            {flipped || matched ? (
+                card
+            ) : (
+                <img src={questionIcon} alt="Question Icon" />
+            )}
         </div>
     );
 };
-
 
 export default Card;
